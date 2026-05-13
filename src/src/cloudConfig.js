ca3119ee0d46fbd86c2c8ad5f726ba590c7cab99
BernardoAraujo <bernardo.m.araujo@gmail.com>
2026-05-13 15:51:16 +0100

    Update cloudConfig.js

--- src/src/cloudConfig.js
+++ src/src/cloudConfig.js
@@ -1,7 +1,7 @@
 // Configuração da ligação ao Google Apps Script.
 // Depois de publicares o Apps Script como Web App, coloca aqui o URL terminado em /exec.
 
-export const CLOUD_API_URL = 'COLOCAR_AQUI_URL_DO_APPS_SCRIPT_WEB_APP'
+export const CLOUD_API_URL = 'https://script.google.com/macros/s/AKfycbw94E0SF-hhCvZ79E2jEHG8cWES4L_hkKt2n2cvswvxk51y4Zne9fDLudNLV1UjiIU_/exec'
 
 // Tem de ser igual ao APP_TOKEN definido no Apps Script.
 export const CLOUD_TOKEN = 'meudiario-2026-token'
